b9ff6aa1ed0ece72ea9126161d9e1b9d92287c0a
fatih <fd1134@gmail.com>
2025-01-21 21:37:00 +0300

    add btnMorelLoader

--- src/main.js
+++ src/main.js
@@ -9,46 +9,107 @@ import SimpleLightbox from 'simplelightbox';
 // Ek stillerin eklenmesi
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import axios from 'axios';
+axios.defaults.baseURL = 'https://pixabay.com/api';
+ let simplo = new SimpleLightbox('.gallery-list a', {
+   captionsData: 'alt',
+   captionDelay: 250,
+ });
 
 const form = document.querySelector('#search-form');
 const loader = document.querySelector('#loader');
+const btnLoadMore = document.querySelector('#load-more');
 const galleryList = document.querySelector('.gallery-list');
 const API_KEY = '48271120-e478f6712aa82518e8481b3a8';
-const url = 'https://pixabay.com/api/';
+let totalPage = 0;
 let params = {
   key: '48271120-e478f6712aa82518e8481b3a8',
   image_type: 'photo',
   orientation: 'horizontal',
   safesearch: true,
+  page: 1,
+  per_page:200
 };
-const fetchPixabay = async (url, params) => {
-  const response = await axios.get(url, { params });
-  return response.data;
-};
-
+btnLoadMore.classList.add('pasif');
 form.addEventListener('submit', async evt => {
   evt.preventDefault();
   loader.classList.add('loader');
   params.q = evt.currentTarget.elements.query.value.trim();
-  fetchPixabay(url, params)
-    .then(data => {
-      if (data.hits.length <= 0) {
+  params.page = 1;
+
+  try {
+    const data = await fetchPixabay(params);
+    if (data.hits.length <= 0) {
+      throw new Error(
+        'Sorry, there are no images matching your search query. Please try again!'
+      );
+    }
+    totalPage = Math.ceil(data.totalHits  / params.per_page);
+    
+    if (totalPage > params.page) {
+       params.page += 1;
+       btnLoadMore.classList.remove('pasif');
+    }
+   
+    
+    const galleryMarkup = markup(data.hits);
+    galleryList.innerHTML = galleryMarkup;
+    loader.classList.remove('loader');
+   
+    simplo.refresh();
+  } catch (error) {
+    iziToast.error({
+      message: `${error}`,
+      position: 'topRight',
+    });
+    galleryList.innerHTML = '';
+    loader.classList.remove('loader');
+  }
+
+  form.reset();
+});
+btnLoadMore.addEventListener("click",async () => {
+  
+  try {
+    const data = await fetchPixabay(params);
+    const galleryMarkup = markup(data.hits);
+    galleryList.insertAdjacentHTML("beforeend",galleryMarkup)
+    // galleryList.innerHTML += galleryMarkup;
+    simplo.refresh();
+    params.page += 1;
+    if (totalPage < params.page) {
+       btnLoadMore.classList.add("pasif");
         throw new Error(
-          'Sorry, there are no images matching your search query. Please try again!'
+          "We're sorry, but you've reached the end of search results"
         );
       }
-      const galleryMarkup = data.hits
-        .map(
-          ({
-            webformatURL,
-            largeImageURL,
-            tags,
-            likes,
-            views,
-            comments,
-            downloads,
-          }) => {
-            return `
+    
+   
+    
+  } catch (error) {
+    iziToast.info({
+      position: 'topRight',
+      message: `${error}`,
+    });
+  }
+  
+});
+const fetchPixabay = async params => {
+  const response = await axios.get('/', { params });
+  return response.data;
+};
+const markup = data => {
+  return data
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return `
             <li class="card">
               <div class="card-image">
                <a class="gallery-link" href="${largeImageURL}">
@@ -74,26 +135,7 @@ form.addEventListener('submit', async evt => {
                </li>
              </ul>
         </li>`;
-          }
-        )
-        .join('');
-      galleryList.innerHTML = galleryMarkup;
-      loader.classList.remove('loader');
-
-      let simplo = new SimpleLightbox('.gallery-list a', {
-        captionsData: 'alt',
-        captionDelay: 250,
-      });
-      simplo.refresh();
-    })
-
-    .catch(error => {
-      iziToast.error({
-        message: `${error}`,
-        position: 'topRight',
-      });
-      galleryList.innerHTML = '';
-      loader.classList.remove('loader');
-    });
-  form.reset();
-});
+      }
+    )
+    .join('');
+};
